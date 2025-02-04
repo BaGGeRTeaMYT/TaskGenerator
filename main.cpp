ea898a92a5d63eba3dfde25453ab26fedb11795c
Andrew <andrey.miclanguss@gmail.com>
2025-02-04 21:26:26 +0300

    Small fix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <TaskProcessor.hpp>
-#include <Consumer.hpp>
+#include <Generator.hpp>
 
 int main() {
     TaskProcessor tmp(std::string(PROJECT_SOURCE_DIR) + std::string("/config.txt"));
-    Consumer cons(tmp, 50);
+    Generator cons(tmp, 50);
     
     cons.write_to_file(std::string(PROJECT_SOURCE_DIR) + std::string("/tmp.tex"));
 
