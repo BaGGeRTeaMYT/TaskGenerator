35ef7fb4f54f38038ceefc95390c8e4fa6b08e9a
Andrew <andrey.miclanguss@gmail.com>
2025-04-08 01:28:28 +0300

    Generation with conditiobs works fine

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 #include <ParserJSON.hpp>
 #include <RandGen.hpp>
 #include <fstream>
+#include <chrono>
 
 int main() {
   std::ofstream output("D:/TaskGenerator/main_out.txt");
   ParserJSON js("D:/TaskGenerator/test_config.json");
   RegexStringGenerator generator;
-  
-  for (int i = 0; i < 1e+3; i++) {
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for (int i = 0; i < 500; i++) {
+    output << "Вариант " << i + 1 << ":\n";
     js.generate_values();
-    std::cout << i << std::endl;
     output << js.to_string();
   }
+
+  auto end = std::chrono::high_resolution_clock::now();
+
+  std::cout << (std::chrono::duration_cast<std::chrono::milliseconds>(end - start)).count() << std::endl;
 
   return 0;
 }
